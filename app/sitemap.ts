54ecfb2c2d4c586d106fe61680f518670f93ce7e
Kannan <kannan@pmsquaresoft.com>
2025-12-01 23:07:53 +0530

    added robots.txt and sitemap.ts for seo support

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://rkannan.space",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}
